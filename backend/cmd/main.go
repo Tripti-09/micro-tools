d91ae6d374825eb6e79f736642230acc5a5f0304
Tripti Singh <triptisd99@gmail.com>
2026-03-13 17:07:41 +0530

    Updated Go Backend

---
+++ backend/cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Backend is running.")
+	})
+
+	fmt.Println("Server running on port: 8080")
+	http.ListenAndServe(":8080", nil)
+
+}
